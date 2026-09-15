dd2e3a3c4f158eed06f4746043c1cee9eede1d92
vitor-fonseca16 <vitorpfonseca16@gmail.com>
2026-09-15 19:53:27 -0300

    Add main menu functionality to main.cpp

--- main.cpp
+++ main.cpp
@@ -20,7 +20,38 @@ void exibir_leituras() {
 }
 	
 int main() {
-	exibir_leituras();
+	int opcao;
+	
+	do{
+	cout << "===== MENU PRINCIPAL =====" << endl;
+	cout << "1. Inserir Leitura" << endl;
+	cout << "2. Calcular Media" << endl;
+	cout << "3. Exibir Leituras" << endl;
+	cout << "4. Sair" << endl;
+	cin >> opcao;
+	
+	switch(opcao){
+		case 1:
+			cout << "Insira a Leitura:" << endl;
+			break;
+		
+		case 2: {
+			cout << "A media calculada e:" << endl;
+			break;
+}		
+		case 3:
+			exibir_leituras() << "As leituras sao:" << endl;
+			break;
+		
+		case 4:
+			cout << "Saida com sucesso" << endl;
+			break;
+		
+		default:
+			cout << "Opcao invalida!" << endl;
+			
+		}
+}	while(opcao !=4);
 	
 	return 0;
 }
